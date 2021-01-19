3386e842e66e9940948bae379ed576a68f893c74
Chen <38539458+jijisduty@users.noreply.github.com>
2021-01-19 16:10:58 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,8 @@
 import requests
 
 def send_msg(text):
-    token = "1464942755:AAHYdVxyuFEXLriq3rW7R_DlN0bs_DsDe6E"         #hide b4 making public
-    chat_id = "-403532973"                                           #hide b4 making public
+    token = "Your token"        
+    chat_id = "Your chat ID"                                           
     url_req = "https://api.telegram.org/bot" + token +"/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
     results = requests.get(url_req)
     print(results.json())

140a21841936e0027007261ede867986023844f7
Chen <38539458+jijisduty@users.noreply.github.com>
2021-01-19 19:09:36 +0200

    Update stock_checker.py

--- stock_checker.py
+++ stock_checker.py
@@ -1,22 +1,31 @@
-import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-from telegram_bot import send_msg
+from Telegbot.telegram_bot import send_msg
+from datetime import date, datetime
 
+todayDate = date.today()
+timeNow = datetime.now()
+
+d2 = todayDate.strftime("%B %d, %Y")
+t2 = timeNow.strftime("%H:%M:%S")
 my_url = 'https://www.bug.co.il/brand/ps5/ps5/console/blue/ray'
+
 # open connection, grab the page
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
+
 #html parsing
 page_soup = soup(page_html, "html.parser")
-
 no_stock = page_soup.findAll("span",{"class":"product-page-no-inventory"})
 yes_stock = page_soup.findAll("span",{"onclick":"addToCart(this,14074);return false;"})
 stock_parent = page_soup.findAll("div",{"class":"span_6_of_12 col float-right-col"})
 
-#Send a telegram message
+#Send a telegram message if in stock, otherwise prints date and time to a log file
 if len(no_stock) != 1:
     send_msg("IN STOCK !!!")
     send_msg("https://www.bug.co.il/brand/ps5/ps5/console/blue/ray")
     print("in stock")
+else:
+    print(t2 + " " + d2 + " " + "Out of stock")
+
